013f8af819b7e3b63b137acefb0c973da84eac48
edu <eduar766@gmail.com>
2023-04-19 12:21:48 -0400

    crear menu option

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../models/models.dart';
 import '../screens/screens.dart';
 
 class AppRoutes {
   static const initialRoute  = 'home';
+
+  static final menuOptions = <MenuOption>[
+    // TODO: Borrar home
+    MenuOption(route: 'home',  name: 'Home Screen', screen: const HomeScreen(), icon: Icons.home_outlined),
+    MenuOption(route: 'listview1',  name: 'ListView Tipo 1', screen: const ListView1Screen(), icon: Icons.list_alt),
+    MenuOption(route: 'listview2',  name: 'ListView Tipo 2', screen: const ListView2Screen(), icon: Icons.list),
+    MenuOption(route: 'alert',  name: 'Alert Screen', screen: const AlertScreen(), icon: Icons.notification_important_outlined),
+    MenuOption(route: 'card',  name: 'Card Screen', screen: const CardScreen(), icon: Icons.credit_card),
+  ];
+
   static Map<String, Widget Function(BuildContext)> routes = {
     'home': (BuildContext context) => const HomeScreen(),
     'listview1': (BuildContext context) => const ListView1Screen(), 
