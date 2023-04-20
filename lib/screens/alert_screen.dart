cd629294eb3a52e1bccf110a8313b2f9282667da
edu <eduar766@gmail.com>
2023-04-20 12:15:38 -0400

    Alert!!

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,14 +1,53 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
+
+  void displayDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false, // Poner en true si queremos que al apretar fuera de la alerta esta se cierre
+      context: context, 
+      builder: (context) {
+        return AlertDialog(
+          elevation: 5,
+          title: const Text('Titulo'),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.circular(10)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('contenido de la alerta'),
+              SizedBox(height: 10,),
+              FlutterLogo(size: 100,)
+            ]
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancelar')
+            )
+          ],
+        );
+      }
+    );
+  }
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-         child: Text('AlertScreen'),
+         child: ElevatedButton(
+          child: const Padding(
+            padding: EdgeInsets.all(20),
+            child: Text('Mostrar Alerta', style: TextStyle(fontSize: 20),),
+          ),
+          onPressed: () => displayDialog(context),
+         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.close),
+        onPressed: () => Navigator.pop(context)
       ),
     );
   }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -11,6 +11,17 @@ class AppTheme {
     ),
     textButtonTheme: TextButtonThemeData(
       style: TextButton.styleFrom(foregroundColor: primary)
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: primary,
+      elevation: 5
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: primary,
+        shape: const StadiumBorder(),
+        elevation: 0
+      )
     )
   );
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
