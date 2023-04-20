7ba1caaa3975167657be3952e18888c57a6b79ce
edu <eduar766@gmail.com>
2023-04-20 11:31:48 -0400

    Pass arguments to Widgets

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -17,11 +17,11 @@ class CardScreen extends StatelessWidget {
         children: const [
           CustomCardType(),
           SizedBox(height: 10,),
-          CustomCardType2(),
+          CustomCardType2(name: 'Paisaje Montañoso',imageUrl: 'https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg',),
           SizedBox(height: 10,),
-          CustomCardType2(),
+          CustomCardType2(name: 'Casa en las praderas', imageUrl: 'https://cdn.shopify.com/s/files/1/0253/6459/4751/articles/2021-fall-photography-hero_1200x.jpg?v=1632425852',),
           SizedBox(height: 10,),
-          CustomCardType2(),
+          CustomCardType2(imageUrl: 'https://assets.churchofjesuschrist.org/0d/85/0d854478366a16ee55554f047db40a6994bb2bf0/colorful_sunset_over_the_mountains.jpeg',),
           SizedBox(height: 100,),
         ],
       )
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,8 +2,14 @@ import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-   
-  const CustomCardType2({Key? key}) : super(key: key);
+  final String imageUrl;
+  final String? name;
+
+  const CustomCardType2({
+    Key? key, 
+    required this.imageUrl, 
+    this.name
+  }) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
@@ -17,19 +23,20 @@ class CustomCardType2 extends StatelessWidget {
           // Image(
           //   image: NetworkImage('https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg')
           //   )
-          const FadeInImage(
-            placeholder: AssetImage('assets/jar-loading.gif'),
-            image: NetworkImage('https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg'),
+          FadeInImage(
+            placeholder: const AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage(imageUrl),
             width: double.infinity,
             height: 230,
             fit: BoxFit.cover,
-            fadeInDuration: Duration(milliseconds: 300),
+            fadeInDuration: const Duration(milliseconds: 300),
           ),
-          Container(
-            alignment: AlignmentDirectional.centerEnd,
-            padding: const EdgeInsets.only(right: 20, top:10, bottom: 10),
-            child: Text('Hermoso paisaje')
-          )
+          if (name != null)
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(right: 20, top:10, bottom: 10),
+              child: Text(name ?? 'No title')
+            )
         ],
       ),
     );
